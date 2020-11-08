de5568eeed418c53fd3f63535317f9e0c6200c4a
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-08 21:51:09 +0200

    Implemented algorithm for finding optimal sequence

---
+++ src/main/java/ua/lviv/iot/searcher/Searcher.java
@@ -0,0 +1,33 @@
+package ua.lviv.iot.searcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ua.lviv.iot.graph.Graph;
+
+public class Searcher {
+
+	public List<String> findOptimalSequenceFor(Graph<String> graph) {
+		int numberOfVertices = graph.numberOfVertices();
+		List<String> optimalSequence = new ArrayList<String>(numberOfVertices);
+		boolean[] isVisited = new boolean[numberOfVertices];
+		for (int counter = 0; counter < numberOfVertices; counter++) {
+			isVisited[counter] = false;
+		}
+		depthFirstSearch(0, graph, optimalSequence, isVisited);
+		return optimalSequence;
+	}
+
+	private void depthFirstSearch(int currentVertexPosition, Graph<String> graph, List<String> optimalSequence,
+			boolean[] isVisited) {
+		if (isVisited[currentVertexPosition]) {
+			return;
+		}
+		isVisited[currentVertexPosition] = true;
+		List<Integer> currentVertexNeighborPositions = graph.getNeighborPositionsFor(currentVertexPosition);
+		for (int neighborPosition = 0; neighborPosition < currentVertexNeighborPositions.size(); neighborPosition++) {
+			depthFirstSearch(currentVertexNeighborPositions.get(neighborPosition), graph, optimalSequence, isVisited);
+		}
+		optimalSequence.add(graph.getVertexAt(currentVertexPosition));
+	}
+}
