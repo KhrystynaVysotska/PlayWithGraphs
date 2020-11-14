62f3fd31a8842286e7bb83b344073d79b3b7c037
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-14 12:20:20 +0200

    Changed structure that implements graph in a more explicit way, deleted unnecessary methods

--- src/main/java/ua/lviv/iot/graph/OrientedGraph.java
+++ src/main/java/ua/lviv/iot/graph/OrientedGraph.java
@@ -1,22 +1,20 @@
 package ua.lviv.iot.graph;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 
 /**
  * An oriented graph build as adjacency list with parameterized vertices.
  * 
  * @author Khrystyna Vysotska
  */
-public class Graph<T> {
-	private int numberOfVertices;
-	private List<T> vertices;
-	private List<List<Integer>> neighborPositions;
+public class OrientedGraph<T> {
+	Map<T, List<T>> orientedGraph;
 
-	public Graph() {
-		vertices = new LinkedList<>();
-		neighborPositions = new LinkedList<>();
+	public OrientedGraph() {
+		orientedGraph = new HashMap<>();
 	}
 
 	/**
@@ -28,7 +26,7 @@ public class Graph<T> {
 	 * @return true if vertex is in graph; false otherwise
 	 */
 	public boolean exists(T vertex) {
-		return vertices.contains(vertex);
+		return orientedGraph.containsKey(vertex);
 	}
 
 	/**
@@ -44,9 +42,7 @@ public class Graph<T> {
 	 */
 	public void addVertex(T vertex) {
 		if (!exists(vertex)) {
-			vertices.add(vertex);
-			neighborPositions.add(new LinkedList<Integer>());
-			numberOfVertices++;
+			orientedGraph.put(vertex, new ArrayList<T>());
 		}
 	}
 
@@ -57,65 +53,25 @@ public class Graph<T> {
 	 *
 	 * @param <T> vertex to draw edge from
 	 * @param <T> vertex to draw edge to
-	 * @throws NoSuchElementException if there is no such vertices in graph
 	 * @return nothing
 	 */
 	public void addEdge(T vertexFrom, T vertexTo) {
-		if (!exists(vertexFrom) || !exists(vertexTo)) {
-			throw new NoSuchElementException();
-		}
-		int vertexFromPosition = getPositionOf(vertexFrom);
-		int vertexToPosition = getPositionOf(vertexTo);
-		neighborPositions.get(vertexFromPosition).add(vertexToPosition);
+		addVertex(vertexFrom);
+		addVertex(vertexTo);
+		orientedGraph.get(vertexFrom).add(vertexTo);
 	}
 
 	/**
 	 * <p>
-	 * Finds positions of neighbors for this vertex position.
+	 * Finds adjacent vertices for this vertex.
 	 * </p>
 	 *
-	 * @param vertexPosition the position of vertex to get neighbors positions for
-	 * @throws NoSuchElementException    if there is no such vertex in the graph
-	 * @throws IndexOutOfBoundsException if the position is out of range
-	 * @return List<Integer> with positions of neighbors for this vertex position
+	 * @param vertex vertex to get adjacent vertices for
+	 * @return List with adjacent vertices for this vertex if this vertex exists;
+	 *         null otherwise
 	 */
-	public List<Integer> getNeighborPositionsFor(int vertexPosition) {
-		if (exists(vertices.get(vertexPosition))) {
-			return neighborPositions.get(vertexPosition);
-		} else {
-			throw new NoSuchElementException();
-		}
-	}
-
-	/**
-	 * <p>
-	 * Finds vertex by its position.
-	 * </p>
-	 *
-	 * @param vertexPosition the position of vertex to find
-	 * @throws NoSuchElementException    if there is no such vertex in the graph
-	 * @throws IndexOutOfBoundsException if the position is out of range
-	 * @return vertex value for this vertex position
-	 */
-	public T getVertexAt(int vertexPosition) {
-		if (exists(vertices.get(vertexPosition))) {
-			return vertices.get(vertexPosition);
-		} else {
-			throw new NoSuchElementException();
-		}
-	}
-
-	/**
-	 * <p>
-	 * Finds position of vertex.
-	 * </p>
-	 *
-	 * @param vertex the vertex to find position for
-	 * @return  vertex position for this vertex value;
-	 *         -1 if there is no such vertex in graph
-	 */
-	public int getPositionOf(T vertex) {
-		return vertices.indexOf(vertex);
+	public List<T> getNeighborsFor(T vertex) {
+		return orientedGraph.get(vertex);
 	}
 
 	/**
@@ -126,7 +82,7 @@ public class Graph<T> {
 	 * @return the number of vertices in graph
 	 */
 	public int numberOfVertices() {
-		return numberOfVertices;
+		return orientedGraph.size();
 	}
 
 	/**
@@ -137,6 +93,6 @@ public class Graph<T> {
 	 * @return true if there is no vertices in graph; false otherwise
 	 */
 	public boolean isEmpty() {
-		return numberOfVertices == 0;
+		return orientedGraph.size() == 0;
 	}
 }
